50ccdf00576bb9fa115704ca48f7eca16c4ed068
unknown <sherxansa@yandex.ru>
2022-08-24 19:58:26 +0300

    init-app

---
+++ src/main/java/com/sher/keywords/KeyWordsApplication.java
@@ -0,0 +1,13 @@
+package com.sher.keywords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyWordsApplication.class, args);
+	}
+
+}
